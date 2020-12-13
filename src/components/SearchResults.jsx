e07c09b41abc1c256e83cb383967c82d7d5f5e69
tsanthosh <santhosh.thamaraiselvan@reedexpo.co.uk>
2020-12-13 19:08:37 +0530

    Bring shows with posters to front in search results

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -5,9 +5,15 @@ import {BingeDetailModel} from "../data/BingeDetailModel";
 import {cssForPhoneAndTablet, Referrer} from "../utils/Constants";
 
 export const SearchResults = ({searchResults, query, onItemClick}) => {
+  let shows = searchResults.map(show => BingeDetailModel(show._source));
+  shows = [
+      ...shows.filter(show => Boolean(show.landscapePoster) || Boolean(show.portraitPoster)),
+      ...shows.filter(show => !Boolean(show.landscapePoster) && !Boolean(show.portraitPoster))
+  ]
+
   return <Container>
     {searchResults.length !== 0 && <Heading>Search results</Heading>}
-    <ShowList shows={searchResults.map(show => BingeDetailModel(show._source))}
+    <ShowList shows={shows}
               referrer={Referrer.SearchResult}
               searchQueryReferrer={query}
               onItemClick={onItemClick}
